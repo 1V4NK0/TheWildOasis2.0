496ebe08cd0415b917c016a3bbaa1aef59359a71
1v4nk0 <1vancherk3s@gmail.com>
2025-02-04 10:13:57 +0000

    added deleting and creating cabins functions

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,15 +9,24 @@ export async function getCabins() {
   return data;
 }
 
-export async function deleteCabin(id) {
-  const { data,error } = await supabase
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
     .from("cabins")
-    .delete()
-    .eq("id", id);
+    .insert([newCabin])
 
-    if (error) {
-      console.log("cabin could not be deleted");
-      throw new Error(error);
-    }
-    return data;
+  if (error) {
+    console.log("cabin could not be created");
+    throw new Error(error);
+  }
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log("cabin could not be deleted");
+    throw new Error(error);
+  }
+  return data;
 }
